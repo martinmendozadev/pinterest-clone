864c9a4f6628ddf7e5b67fb777c2b4850b8e4fc6
Martin Mendoza <marttcode@gmail.com>
2020-03-24 23:06:27 -0600

    Making an App Posts

--- marttgram/urls.py
+++ marttgram/urls.py
@@ -3,12 +3,15 @@
 from django.urls import path
 from django.http import HttpResponse
 
-from marttgram import views
+from marttgram import views as local_views
+from posts import views as posts_views
 
 
 urlpatterns = [
-    path('hello_word/', views.hello_word),
-    path('get/', views.get),
-    path('check_point/<str:name>/<int:age>/', views.check_point),
+    path('hello_word/', local_views.hello_word),
+    path('get/', local_views.get),
+    path('check_point/<str:name>/<int:age>/', local_views.check_point),
+
+    path('posts/', posts_views.list_posts),
 
 ]
---
+++ posts/apps.py
@@ -0,0 +1,10 @@
+"""Posts aplication module"""
+
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Posts aplication settings."""
+    
+    name = 'posts'
+    verbose_name = 'Posts'

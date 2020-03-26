5de864e2004547bad1924d76bb1e9fb04bea5080
Martin Mendoza <marttcode@gmail.com>
2020-03-26 01:32:49 -0600

    Fix error update Django

--- users/admin.py
+++ users/admin.py
@@ -11,4 +11,21 @@ from users.models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     """Profile admin."""
 
-    pass
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user',)
+    list_editable = ('phone_number', 'website', 'picture')
+
+    search_fields = (
+        'user__email',
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+    )
+
+    list_filter = (
+        'user__is_active',
+        'user__is_staff',
+        'created',
+        'modified',
+    )
